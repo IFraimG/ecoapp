fe855f454d850aa2508cf0f5c2a856c8cdf73c7a
IFraimG <kulsasha55@gmail.com>
2023-09-16 20:03:27 +0300

    update nav_graph

--- app/src/main/java/com/example/ecoapp/fragments/MapFragment.java
+++ app/src/main/java/com/example/ecoapp/fragments/MapFragment.java
@@ -1,7 +1,18 @@
 package com.example.ecoapp.fragments;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Build;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -11,6 +22,7 @@ import com.example.ecoapp.databinding.FragmentMapBinding;
 import com.yandex.mapkit.Animation;
 import com.yandex.mapkit.MapKitFactory;
 import com.yandex.mapkit.geometry.Point;
+import com.yandex.mapkit.location.LocationStatus;
 import com.yandex.mapkit.map.CameraPosition;
 import com.yandex.mapkit.mapview.MapView;
 import com.yandex.mapkit.user_location.UserLocationLayer;
@@ -22,6 +34,7 @@ public class MapFragment extends Fragment {
     private MapView mapView;
     private final Animation pingAnimation = new Animation(Animation.Type.SMOOTH, 0);
     private UserLocationLayer userLocationLayer;
+    private LocationManager locationManager;
 
     @Override
     public View onCreateView(@NotNull LayoutInflater inflater, ViewGroup container,
@@ -38,6 +51,7 @@ public class MapFragment extends Fragment {
         mapView.getMap().setRotateGesturesEnabled(false);
         mapView.getMap().move(new CameraPosition(new Point(55.71989101308894, 37.5689757769603), 14, 0, 0), pingAnimation, null);
 
+        locationManager = (LocationManager) requireActivity().getSystemService(Context.LOCATION_SERVICE);
 
         return binding.getRoot();
     }
@@ -53,6 +67,41 @@ public class MapFragment extends Fragment {
     public void onStop() {
         super.onStop();
         binding.mapview.onStop();
+//        if (locationListener != null) locationManager.unsubscribe(locationListener);
         MapKitFactory.getInstance().onStop();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        ActivityCompat.requestPermissions(requireActivity(),
+                new String[]{
+                        android.Manifest.permission.ACCESS_FINE_LOCATION,
+                        android.Manifest.permission.ACCESS_COARSE_LOCATION,
+                }, 200);
+
+        if (ContextCompat.checkSelfPermission(requireActivity(), Manifest.permission.ACCESS_BACKGROUND_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(requireActivity(),
+                    new String[]{ android.Manifest.permission.ACCESS_BACKGROUND_LOCATION }, 201);
+        }
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,
+                1000 * 10, 10, locationListener);
+        locationManager.requestLocationUpdates(
+                LocationManager.NETWORK_PROVIDER, 1000 * 10, 10,
+                locationListener);
+    }
+
+    private LocationListener locationListener = new LocationListener() {
+        @Override
+        public void onLocationChanged(@NonNull Location location) {
+
+        }
+
+        @Override
+        public void onProviderEnabled(String provider) {
+//            checkEnabled();
+//            showLocation(locationManager.getLastKnownLocation(provider));
+        }
     }
 }
