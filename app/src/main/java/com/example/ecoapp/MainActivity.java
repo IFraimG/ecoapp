23807ce2f2b11ad8e9c11560042980642595366d
Sofiya Boyko <sonya.bojkovk@mail.ru>
2023-09-16 20:02:31 +0300

    Create Event Fragment in Java was modified the second time

--- app/src/main/java/com/example/ecoapp/MainActivity.java
+++ app/src/main/java/com/example/ecoapp/MainActivity.java
@@ -76,5 +76,4 @@ public class MainActivity extends AppCompatActivity {
             return true;
         });
     }
-
 }
--- app/src/main/java/com/example/ecoapp/fragments/CreateEventFragment.java
+++ app/src/main/java/com/example/ecoapp/fragments/CreateEventFragment.java
@@ -3,15 +3,14 @@ package com.example.ecoapp.fragments;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
-import androidx.navigation.Navigation;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.example.ecoapp.MainActivity;
+import com.example.ecoapp.databinding.FragmentCreateEventBinding;
 import com.example.ecoapp.databinding.FragmentEventsBinding;
 import com.example.ecoapp.R;
 
@@ -19,14 +18,18 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class CreateEventFragment extends Fragment {
-    private FragmentEventsBinding fragmentEventsBinding;
+    private FragmentCreateEventBinding fragmentCreateEventBinding;
+    private Button createEventButton;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        fragmentEventsBinding = FragmentEventsBinding.inflate(getLayoutInflater());
+        fragmentCreateEventBinding = FragmentCreateEventBinding.inflate(getLayoutInflater());
 
+        fragmentCreateEventBinding.createEventButtonFragmentCreateEvent.setOnClickListener(v -> {
+            Navigation.findNavController(v).navigate(R.id.action_eventsFragment_to_mapFragment);
+        });
 
-        return fragmentEventsBinding.getRoot();
+        return fragmentCreateEventBinding.getRoot();
     }
 }
