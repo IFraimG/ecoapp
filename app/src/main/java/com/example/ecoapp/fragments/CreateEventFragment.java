867bad7cc63f0e11c852e3bf31e6a7a0226f4f26
Sofiya Boyko <sonya.bojkovk@mail.ru>
2023-09-23 18:00:33 +0300

    Selecting of photo is in progress

--- app/src/main/java/com/example/ecoapp/fragments/CreateEventFragment.java
+++ app/src/main/java/com/example/ecoapp/fragments/CreateEventFragment.java
@@ -1,11 +1,20 @@
 package com.example.ecoapp.fragments;
 
+import static android.app.Activity.RESULT_OK;
+
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
 
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.navigation.Navigation;
 
+import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -17,6 +26,11 @@ import com.example.ecoapp.presentation.viewmodels.EventViewModel;
 
 import org.jetbrains.annotations.NotNull;
 
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Locale;
@@ -24,6 +38,10 @@ import java.util.Locale;
 public class CreateEventFragment extends Fragment {
     private FragmentCreateEventBinding fragmentCreateEventBinding;
     private EventViewModel eventViewModel;
+
+    private int SELECT_PHOTO_PROFILE = 1;
+
+    private Uri uri;
 
     @Override
     public View onCreateView(@NotNull LayoutInflater inflater, ViewGroup container,
@@ -60,7 +78,67 @@ public class CreateEventFragment extends Fragment {
             }
         });
 
+        fragmentCreateEventBinding.createEventPhoto.setOnClickListener(v -> {
+            Intent intent = new Intent(Intent.ACTION_PICK);
+            intent.setType("image/*");
+
+            Intent chooserIntent = Intent.createChooser(intent, "Choose Photo");
+
+            Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            chooserIntent.putExtra(Intent.EXTRA_INITIAL_INTENTS, new Intent[]{cameraIntent});
+
+            startActivityForResult(chooserIntent, SELECT_PHOTO_PROFILE);
+        });
+
         return fragmentCreateEventBinding.getRoot();
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (((requestCode == SELECT_PHOTO_PROFILE && resultCode == RESULT_OK)) && data != null) {
+            if (data.getData() != null) {
+                uri = data.getData();
+
+                try {
+                    final InputStream imageStream = requireActivity().getContentResolver().openInputStream(uri);
+                    final Bitmap originalBitmap = BitmapFactory.decodeStream(imageStream);
+
+                    String[] filePathColumn = { MediaStore.Images.Media.DATA };
+                    Cursor cursor = requireActivity().getContentResolver().query(uri, filePathColumn, null, null, null);
+                    cursor.moveToFirst();
+
+                    int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+                    String imagePath = cursor.getString(columnIndex);
+                    cursor.close();
+
+                    File file = new File(imagePath);
+                    //saveImage(file, originalBitmap);
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            } else {
+                Bundle extras = data.getExtras();
+                Bitmap bitmap = (Bitmap) extras.get("data");
+                File f = new File(requireContext().getCacheDir(), "test");
+                try {
+                    f.createNewFile();
+                    ByteArrayOutputStream bos = new ByteArrayOutputStream();
+                    bitmap.compress(Bitmap.CompressFormat.PNG, 0, bos);
+
+                    byte[] bitmapdata = bos.toByteArray();
+
+                    FileOutputStream fos = new FileOutputStream(f);
+                    fos.write(bitmapdata);
+                    fos.flush();
+                    fos.close();
+
+                    //saveImage(f, bitmap);
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }
     }
 
     public boolean isValidDateFormat(String inputDate) {
@@ -86,5 +164,4 @@ public class CreateEventFragment extends Fragment {
             return false;
         }
     }
-
 }
