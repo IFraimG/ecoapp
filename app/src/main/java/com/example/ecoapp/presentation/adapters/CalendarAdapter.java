3a61175b84a920dd19f31fb81d332be8f349be76
Sofiya Boyko <sonya.bojkovk@mail.ru>
2023-10-14 18:39:37 +0300

    Calendar was added

---
+++ app/src/main/java/com/example/ecoapp/presentation/adapters/CalendarAdapter.java
@@ -0,0 +1,70 @@
+package com.example.ecoapp.presentation.adapters;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.TextView;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+class CalendarAdapter extends RecyclerView.Adapter<CalendarAdapter.CalendarViewHolder>
+{
+    private final ArrayList<String> daysOfMonth;
+    private final OnItemListener onItemListener;
+
+    public CalendarAdapter(ArrayList<String> daysOfMonth, OnItemListener onItemListener)
+    {
+        this.daysOfMonth = daysOfMonth;
+        this.onItemListener = onItemListener;
+    }
+
+    @NonNull
+    @Override
+    public CalendarViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType)
+    {
+        LayoutInflater inflater = LayoutInflater.from(parent.getContext());
+        View view = inflater.inflate(R.layout.calendar_cell, parent, false);
+        ViewGroup.LayoutParams layoutParams = view.getLayoutParams();
+        layoutParams.height = (int) (parent.getHeight() * 0.166666666);
+        return new CalendarViewHolder(view, onItemListener);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull CalendarViewHolder holder, int position)
+    {
+        holder.dayOfMonth.setText(daysOfMonth.get(position));
+    }
+
+    @Override
+    public int getItemCount()
+    {
+        return daysOfMonth.size();
+    }
+
+    public interface  OnItemListener
+    {
+        void onItemClick(int position, String dayText);
+    }
+
+    public class CalendarViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener
+    {
+        public final TextView dayOfMonth;
+        private final CalendarAdapter.OnItemListener onItemListener;
+        public CalendarViewHolder(@NonNull View itemView, CalendarAdapter.OnItemListener onItemListener)
+        {
+            super(itemView);
+            dayOfMonth = itemView.findViewById(R.id.cellDayText);
+            this.onItemListener = onItemListener;
+            itemView.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View view)
+        {
+            onItemListener.onItemClick(getAdapterPosition(), (String) dayOfMonth.getText());
+        }
+    }
+}
