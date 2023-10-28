7f77d2e38c9687e87e843b72cf242737be999f54
Sofiya Boyko <sonya.bojkovk@mail.ru>
2023-10-28 19:14:23 +0300

    search... ^,^

--- app/src/main/java/com/example/ecoapp/presentation/fragments/SearchFragment.java
+++ app/src/main/java/com/example/ecoapp/presentation/fragments/SearchFragment.java
@@ -8,21 +8,26 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import com.example.ecoapp.R;
+import com.example.ecoapp.databinding.FragmentEventBinding;
 import com.example.ecoapp.databinding.FragmentSearchBinding;
-import com.example.ecoapp.domain.helpers.StorageHandler;
-
-import org.jetbrains.annotations.NotNull;
+import com.google.android.material.search.SearchView;
 
 public class SearchFragment extends Fragment {
     private FragmentSearchBinding binding;
-
+    private SearchView searchView;
+    private ListView listView;
     @Override
-    public View onCreateView(@NotNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_search, container, false);
-        binding.setThemeInfo(new StorageHandler(requireContext()).getTheme());
-
-        return binding.getRoot();
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        binding = FragmentSearchtBinding.inflate(getLayoutInflater());
+        String[] items = {
+                "Apple", "Banana", "Cherry", "Date", "Fig", "Grape", "Kiwi", "Lemon", "Mango",
+                "Orange", "Papaya", "Peach", "Pear", "Pineapple", "Plum", "Raspberry", "Strawberry",
+                "Tangerine", "Watermelon"
+        };
+        return inflater.inflate(R.layout.fragment_search, container, false);
     }
 }
